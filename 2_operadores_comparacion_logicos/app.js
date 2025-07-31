88cea19c935ac15a9e16807b550420b809eb1bd9
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-07-31 13:28:27 -0500

    Ejercicio terminado

--- 2_operadores_comparacion_logicos/app.js
+++ 2_operadores_comparacion_logicos/app.js
@@ -19,9 +19,24 @@ let n1 = Number (prompt('Ingresa un valor'))
 let n2 = Number (prompt('Ingresa segundo valor'))
 let resultado1 = n1
 let resultado2 = n2
+let resultado3 = n1+n2
+let resultado4 = n1-n2
+let resultado5 = n1*n2
+let resultado6 = n1/n2
 alert("El valor uno fue" + resultado1)
 alert("El valor dos fue" + resultado2)
 
 
+//alert('Suma de ambos valores' + resultado3)
+//alert('Resta de ambos valores' + resultado4)
+//alert('Multiplicación de ambos valores' + resultado5)
+//alert('La división de ambos resultados' + resultado6)
 
+//es  mejor el console.log porque arroja los datos de una vez de la operaciones, 
+// con alerta también se puede pero pide muchas veces enter para continuar con los resultados
+
+
+//SOLUCIÓN DE EJERCICIO CONCATENANDO (+) 
+console.log(`Tu numero es ${n1}`)
+// Sirve para no poner todos los + para citar texto con variables
 
