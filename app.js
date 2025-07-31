4a81dc6c12c2f48b363d5e0215c3b7c2ffc80a8c
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-07-31 12:26:00 -0500

    Repositorio js versión 1

---
+++ app.js
@@ -0,0 +1,27 @@
+//boolean(...)
+
+//console.log(' "5" == "5" ', "5" == "5") //true
+//console.log(' "5" === "5" ', "5" === "5") //true
+//console.log(' 5 === "5" ', 5 === "5") //false
+//console.log(' 5 == "5" ', 5 == "5") //true
+//console.log(' 5 === (3+2) ', "5" === (3+2) ) //false
+//console.log(' 5 === 5 ', 5 === 5) //true
+
+//Pida dos números al usuario y sumelos
+//let num1 = Number (prompt('Dame num 1'))
+//let num2 = Number (prompt('Dame num 2'))
+//let resultado = num1 + num2
+//alert("El resultado de tu suma es" + resultado)
+
+//Ejercicio de clase
+
+let n1 = Number (prompt('Ingresa un valor'))
+let n2 = Number (prompt('Ingresa segundo valor'))
+let resultado1 = n1
+let resultado2 = n2
+alert("El valor uno fue" + resultado1)
+alert("El valor dos fue" + resultado2)
+
+
+
+
