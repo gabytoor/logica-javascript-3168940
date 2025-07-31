50a1b30aed8507eb21abc415c959c1b6dfc33cfd
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-07-31 12:45:43 -0500

    Add files via upload

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,29 @@
+//El "alert" muestra una  ventana emergente en el navegador//
+//alert("Hola Mundo");
+
+//El "prompt" muestra una caja para mostrar informacion desde el navegador//
+//prompt("Dime tu nombre")
+
+//Variable declarada con let//
+let personaje="perrito"
+
+//Crear variale puntaje y asisgnar valor de 0 (Los valores numericos van sin comillas)//
+let puntaje= 0 
+
+//Crear variale shots y asisgnar valor de 3 (Los valores numericos van sin comillas)//
+let shots= 3 
+    recarga= 2
+
+console.log("Total de Balas:", shots+recarga)
+
+//El usuario capturo 4 patos//
+puntaje += 4
+//El usuario gasto 2 balas//
+shots -= 2
+//El usuario recargo su arma//
+recarga += 1
+recarga++ //++ permite sumar de a uno//
+
+console.log("puntaje:",puntaje) //4//
+console.log("shots:",shots) //1//
+console.log("recarga:",recarga) //3//
