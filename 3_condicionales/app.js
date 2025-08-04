1a142f2d6d44feca0fc49a55e42b7aadbce751ba
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-08-04 13:33:09 -0500

    Avance at

---
+++ 3_condicionales/app.js
@@ -0,0 +1,16 @@
+
+let edad = (prompt ('Ingresa tu edad'))
+
+if (edad >= 18) 
+    alert ("Puedes entrar a nuestra discoteca, ¡Disfruta!")
+
+else if (edad < 18)
+    alert ("No puedes ingresar a nuestra disco por tu edad ")
+
+let nombre = (prompt ('Ingresa tu nombre'))
+
+if (nombre.toLowerCase () === ("Mario"))
+    alert ("Es noche de Mario's, ¡Tienes acceso gratis a VIP!")
+
+if (nombre.toLowerCase() == "Carlos")
+    alert ("Es noche de Carlos's, ¡Tienes acceso gratis a VIP!")
