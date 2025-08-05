e65227af0cbac38569a67864185c92cbad67baa4
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-08-05 13:26:51 -0500

    Add files via upload

---
+++ 4_ciclos_arrays/app.js
@@ -0,0 +1,19 @@
+//Array
+let frutas = [mango,"piña","papaya","uva","limón","aguacate"]
+let jugadores = [
+  {
+    id: 1,
+    nombre: "diaz", 
+    pais: "col"
+  },
+    "james",
+    "messi"
+  ]
+
+//Objeto
+let mango = {
+  nombre: "Back to the future",
+  estreno: 1986,
+  director:  "Robert Zemeckis",
+  calificacion: "8.5"
+}
