f09afb235fb2f62d67f4f36c498b9545bb909083
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-08-11 21:45:36 -0500

    Add files via upload
    Actualización solución actividad

--- 3_condicionales/app.js
+++ 3_condicionales/app.js
@@ -1,16 +1,51 @@
+// ACTIVIDAD 1
 
-let edad = (prompt ('Ingresa tu edad'))
+/*
+let nombre = prompt ('Ingresa tu nombre').toLowerCase() .trim();
+let edad =  parseInt (prompt ('Ingresa tu edad'));
 
-if (edad >= 18) 
-    alert ("Puedes entrar a nuestra discoteca, ¡Disfruta!")
 
-else if (edad < 18)
-    alert ("No puedes ingresar a nuestra disco por tu edad ")
+if (edad>=18){ alert('Puedes ingresar a la discoteca');}
 
-let nombre = (prompt ('Ingresa tu nombre'))
+else if(edad<18){ alert('No puedes ingresar por tu edad');}
 
-if (nombre.toLowerCase () === ("Mario"))
-    alert ("Es noche de Mario's, ¡Tienes acceso gratis a VIP!")
 
-if (nombre.toLowerCase() == "Carlos")
-    alert ("Es noche de Carlos's, ¡Tienes acceso gratis a VIP!")
+if (nombre == "mario" || nombre == "carlos") { alert('Tienes acceso a VIP');}
+else {alert('Disfruta la noche');}
+*/
+
+
+
+
+
+// ACTIVIDAD 2
+
+
+let estatura = Number(prompt ('Ingresa tu estatura en cm'));
+let velocidad = Number(prompt ('¿Qué velocidad alcanzas al correr en km/h?'));
+
+let estatura_requerida = Number(170);
+let velocidad_requerida = Number(27);
+
+
+if ((estatura>=estatura_requerida) && (velocidad>=velocidad_requerida)){alert("¡Felicidades!, pasas el primer filtro");
+
+
+let edad = Number(prompt('Ingresa tu edad'));
+let edad_requerida = Number (18);
+
+if (edad >= edad_requerida) {alert("Entras a las divisones mayores");}
+else {alert("Entras a las divisones menores");}}
+
+else {alert("Lo siento, no cumples con las condiciones para entrar al equipo");}
+
+
+
+
+
+
+
+
+
+
+
